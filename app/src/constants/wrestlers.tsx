5950fa6c5eb3ddd515c076d18caf6454fea7ab91
Jeffery <jeffxie27@outlook.com>
2023-09-06 17:32:50 -0600

    Wrestlers file that stores all basic information about each wrestler.

---
+++ app/src/constants/wrestlers.tsx
@@ -0,0 +1,161 @@
+import beckylynch from '../../public/images/becky.jpg';
+import brittbaker from '../../public/images/britt.jpg';
+import charlotteflair from '../../public/images/charlotte.jpg';
+import cmpunk from '../../public/images/cmpunk.jpg';
+import ioshirai from '../../public/images/ioshirai.jpg';
+import jeffhardy from '../../public/images/jeffhardy.jpg';
+import johnnygargano from '../../public/images/johnny.jpg';
+import kennyomega from '../../public/images/kennyomega.jpg';
+import rubysoho from '../../public/images/rubysoho.png';
+import sashabanks from '../../public/images/sasha.jpg';
+import shinsukenakamura from '../../public/images/shinsuke.jpg';
+import tommasociampa from '../../public/images/ciampa.jpg';
+
+const wrestlers = [
+  {
+    name: "Becky Lynch",
+    nickname: "The Man",
+    url: "beckylynch",
+    gender: "Female",
+    weight: "130 lbs (59 kg)",
+    height: `5' 6" (168 cm)`,
+    hometown: "Dublin, Ireland",
+    social: "https://twitter.com/BeckyLynchWWE",
+    image: beckylynch.src,
+    color: "orange"
+  },
+  {
+    name: "Britt Baker",
+    nickname: "DMD",
+    url: "brittbaker",
+    gender: "Female",
+    weight: "121 lbs (55 kg)",
+    height: `5' 7" (170 cm)`,
+    hometown: "Punxsatawney, Pennsylvania, USA",
+    social: "https://twitter.com/realbrittbaker",
+    image: brittbaker.src,
+    color: "red"
+  },
+  {
+    name: "Charlotte Flair",
+    nickname: "The Queen",
+    url: "charlotteflair",
+    gender: "Female",
+    weight: "143 lbs (65 kg)",
+    height: `5' 10" (178 cm)`,
+    hometown: "Charlotte, North Carolina, USA",
+    social: "http://twitter.com/MsCharlotteWWE",
+    image: charlotteflair.src,
+    color: "blue"
+  },
+  {
+    name: "CM Punk",
+    nickname: "The Best In The World",
+    url: "cmpunk",
+    gender: "Male",
+    weight: "218 lbs (99 kg)",
+    height: `6' 0" (183 cm)`,
+    hometown: "Chicago, Illinois, USA",
+    social: "http://twitter.com/CMPUNK",
+    image: cmpunk.src,
+    color: "red"
+  },
+  {
+    name: "Io Shirai",
+    nickname: "The Genius Of The Sky",
+    url: "ioshirai",
+    gender: "Female",
+    weight: "121 lbs (55 kg)",
+    height: `5' 1" (156 cm)`,
+    hometown: "Kamakura, Kanagawa, Japan",
+    social: "https://twitter.com/Shirai_Io",
+    image: ioshirai.src,
+    color: "purple"
+  },
+  {
+    name: "Jeff Hardy",
+    nickname: "The Charismatic Enigma",
+    url: "jeffhardy",
+    gender: "Male",
+    weight: "218 lbs (99 kg)",
+    height: `6' 1" (185 cm)`,
+    hometown: "Cameron, North Carolina, USA",
+    social: "http://twitter.com/#!/JEFFHARDYBRAND",
+    image: jeffhardy.src,
+    color: "purple"
+  },
+  {
+    name: "Johnny Gargano",
+    nickname: "Johnny Wrestling",
+    url: "johnnygargano",
+    gender: "Male",
+    weight: "198 lbs (90 kg)",
+    height: `5' 10" (178 cm)`,
+    hometown: "Lakewood, Ohio, USA",
+    social: "http://www.twitter.com/JohnnyGargano",
+    image: johnnygargano.src,
+    color: "darkblue"
+  },
+  {
+    name: "Kenny Omega",
+    nickname: "The Cleaner",
+    url: "kennyomega",
+    gender: "Male",
+    weight: "218 lbs (99 kg)",
+    height: `6' 0" (183 cm)`,
+    hometown: "Winnipeg, Manitoba, Canada",
+    social: "https://twitter.com/KennyOmegamanX",
+    image: kennyomega.src,
+    color: "blue"
+  },
+  {
+    name: "Ruby Soho",
+    nickname: "The Runaway",
+    url: "rubysoho",
+    gender: "Female",
+    weight: "121 lbs (55 kg)",
+    height: `5' 4" (163 cm)`,
+    hometown: "Edwardsburg, Michigan, USA",
+    social: "https://twitter.com/realrubysoho",
+    image: rubysoho.src,
+    color: "green"
+  },
+  {
+    name: "Sasha Banks",
+    nickname: "The Boss",
+    url: "sashabanks",
+    gender: "Female",
+    weight: "114 lbs (52 kg)",
+    height: `5' 5" (164 cm)`,
+    hometown: "Fairfield, California, USA",
+    social: "https://twitter.com/SashaBanksWWE",
+    image: sashabanks.src,
+    color: "purple"
+  },
+  {
+    name: "Shinsuke Nakamura",
+    nickname: "The King Of Strong-Style",
+    url: "shinsukenakamura",
+    gender: "Male",
+    weight: "220 lbs (100 kg)",
+    height: `6' 2" (188 cm)`,
+    hometown: "Mineyama, Kyoto, Japan",
+    social: "http://twitter.com/#!/ShinsukeN",
+    image: shinsukenakamura.src,
+    color: "red"
+  },
+  {
+    name: "Tommaso Ciampa",
+    nickname: "Blackheart",
+    url: "tommasociampa",
+    gender: "Male",
+    weight: "205 lbs (93 kg)",
+    height: `6' 0" (183 cm)`,
+    hometown: "Everett, Massachusetts, USA",
+    social: "https://twitter.com/NXTCiampa",
+    image: tommasociampa.src,
+    color: "black"
+  }
+]
+
+export default wrestlers;
