f40bdfa174d14dc46875599fecbc0066e0ba1e76
Jeffery <jeffxie27@outlook.com>
2023-09-21 19:35:02 -0600

    Removed metadata from the pages layout file.

--- app/src/pages/layout.tsx
+++ app/src/pages/layout.tsx
@@ -7,11 +7,6 @@ import type { Metadata } from 'next';
 // Components
 import Navbar from '@/components/navbar';
 
-export const metadata: Metadata = {
-  title: 'Fight Forever',
-  description: 'Welcome to FightForeverX, your one-stop destination for all things wrestling! Dive into the electrifying world of professional wrestling with our comprehensive platform that offers a wealth of information on your favourite wrestlers, their captivating statistics, upcoming pay-per-view events, and the latest wrestling news.',
-}
-
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
