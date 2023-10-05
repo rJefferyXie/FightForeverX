c1a48e41c9e15c11da382f586577ed9f8a8b65ee
Jeffery <jeffxie27@outlook.com>
2023-10-05 17:50:40 -0600

    Fixed articles styling.

--- app/src/components/articles.tsx
+++ app/src/components/articles.tsx
@@ -10,13 +10,13 @@ const Articles = (props: React.PropsWithChildren<ArticlesProps>) => {
   const { articles, setCurrentArticle } = props;
 
   return (
-    <div className="flex flex-wrap justify-center h-[27rem] overflow-hidden">
-      {articles.length && 
+    <div className="flex flex-wrap justify-center h-[29rem] overflow-hidden">
+      {articles &&
         articles.map((article: Article, idx: number) => {
           return (
             <div 
               key={idx}
-              className="flex sm:flex-col drop-shadow-md text-black border-slate-300 border-b p-2 sm:p-0 sm:m-2 h-[8rem] sm:w-[27rem] sm:h-[26rem] sm:rounded-md bg-white"
+              className="flex sm:flex-col drop-shadow-sm text-black p-2 sm:p-0 sm:m-2 h-[8rem] sm:w-[28rem] sm:h-[28rem] sm:rounded-md bg-white shadow"
             >
               <img
                 className="w-40 sm:w-full sm:rounded-t-md"
@@ -24,22 +24,16 @@ const Articles = (props: React.PropsWithChildren<ArticlesProps>) => {
                 alt={article.title}
               />
 
-              <div className="flex flex-col overflow-hidden">
-                <p className="font-semibold mx-4 sm:mt-1 line-clamp-3">
+              <div className="flex flex-col h-full overflow-hidden">
+                <p className="font-semibold mx-3 sm:mt-1 line-clamp-2">
                   {article.title}
                 </p>
 
-                <p className="hidden sm:line-clamp-3 mx-4 mb-1">
-                  {article.text.map((text: string, idx: number) => {
-                    return (
-                      <p key={idx}>
-                        {text}
-                      </p>
-                    )
-                  })}                    
+                <p className="hidden sm:line-clamp-4 mx-3 pb-1">
+                  {article.preview}                  
                 </p>
 
-                <div className="flex justify-center mt-auto mb-1">
+                <div className="flex justify-center mt-auto mb-4">
                   <button 
                     className="px-2 py-1 mx-2 rounded-md bg-sky-700 text-white hover:bg-sky-600 duration-200 ease-in-out"
                     onClick={() => setCurrentArticle(article)}
