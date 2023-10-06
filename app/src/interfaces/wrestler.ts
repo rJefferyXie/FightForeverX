25d1c94e47bd337faac6560e03de20c8ff8b136e
Jeffery <jeffxie27@outlook.com>
2023-10-06 15:57:31 -0600

    Interface for wrestlers.

---
+++ app/src/interfaces/wrestler.ts
@@ -0,0 +1,14 @@
+interface Wrestler {
+  name: string,
+  nickname: string,
+  url: string,
+  gender: string,
+  weight: string,
+  height: string,
+  hometown: string,
+  social: string,
+  image: string,
+  color: string
+}
+
+export default Wrestler;
