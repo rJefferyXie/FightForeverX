c146badf1872abe53265e7508be577625c53639f
Jeffery <jeffxie27@outlook.com>
2023-10-31 15:53:48 -0600

    Can see basic wrestler information.

--- app/src/pages/wrestler/[wrestler].tsx
+++ app/src/pages/wrestler/[wrestler].tsx
@@ -1,6 +1,115 @@
+"use client"
+
+// Next + React
+import { useEffect, useState } from 'react';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import WrestlerActions from '@/store/actions/wrestlerActions';
+
+// Firebase
+import { db } from '@/firebase/config';
+import { collection, doc, getDocs, query, orderBy } from "firebase/firestore"; 
+
 const Wrestler = () => {
+  const [matches, setMatches] = useState<any[]>([]);
+
+  // Redux
+  const dispatch = useDispatch();
+  const wrestlerState = useSelector((state: any) => state.wrestler.currentWrestler);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const wrestlersRef = collection(db, "wrestlers");
+        const wrestlerRef = doc(wrestlersRef, wrestlerState.url);
+        const matchesRef = collection(wrestlerRef, "matches");
+
+        const wrestlerMatches: any[] = [];
+        const querySnapshot = await getDocs(matchesRef);
+        querySnapshot.forEach(async (matchDoc) => {
+          const matchData = matchDoc.data();
+          wrestlerMatches.push(matchData);
+        });
+
+        setMatches(wrestlerMatches);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getData();
+  }, []);
+
   return (
-    <div>Hi</div>
+    <div className="flex flex-col h-screen w-screen overflow-y-auto bg-zinc-50 text-black lg:ml-20 sm:p-4">
+      <p className="font-semibold text-xl mx-auto">
+        {wrestlerState.name}
+      </p>
+
+      <p 
+        className="font-semibold text-white w-fit px-2 py-1 rounded-md mx-auto my-2"
+        style={{backgroundColor: wrestlerState.color}}
+      >
+        {wrestlerState.nickname}
+      </p>
+
+      <p>
+        {wrestlerState.height}
+      </p>
+
+      <p>
+        {wrestlerState.weight}
+      </p>
+
+      <p>
+        {wrestlerState.hometown}
+      </p>
+
+      <p>
+        {wrestlerState.gender}
+      </p>
+
+      <a 
+        href={wrestlerState.social}                     
+        target="_blank"       
+        rel="noopener noreferrer"
+      >
+        {wrestlerState.social}
+      </a>
+
+      <img
+        className="w-64 rounded-md"
+        src={wrestlerState.image}
+        alt={"An image of " + wrestlerState.name}
+      />
+
+      {matches.length > 0 && 
+        matches.map((match, idx: number) => {
+          return (
+            <div 
+              key={idx} 
+              className="my-1 p-1 text-white text-shadow"
+              style={{backgroundColor: match.wonMatch ? "green" : "red"}}
+            >
+              <p>
+                {match.eventName}
+              </p>
+
+              <p>
+                {match.eventDetails}
+              </p>
+
+              <p>
+                {match.eventResult}
+              </p>
+            </div>
+          )
+        })
+      }
+
+
+    </div>
   )
 }
 
--- app/src/pages/wrestlers.tsx
+++ app/src/pages/wrestlers.tsx
@@ -1,37 +1,58 @@
-// Styles
-import '../app/globals.scss';
+"use client"
 
 // React + Next
 import ExportedImage from "next-image-export-optimizer";
-import MainLayout from './layout';
+import { useRouter } from 'next/router';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import WrestlerActions from '@/store/actions/wrestlerActions';
 
 // Constants
 import wrestlers from '../constants/wrestlers';
 
-const Wrestlers = () => {
-  return (
-    <MainLayout>
-      <main className="flex flex-wrap content-center justify-center h-full sm:h-screen w-screen bg-zinc-50 sm:ml-20 sm:p-4">
-        {wrestlers.map((wrestler, idx) => {
-          return (
-            <div className={"wrestler " + wrestler.color} key={idx}>
-              <div className="wrestler-name-container">
-                <p className="wrestler-name">{wrestler.name}</p>
-                <p className="wrestler-nickname">{wrestler.nickname}</p>
-              </div>
+// Interfaces
+import Wrestler from '@/interfaces/wrestler';
 
-              <ExportedImage
-                className="wrestler-image"
-                src={wrestler.image}
-                alt={"An image of " + wrestler.name}
-                height={64}
-                width={64}
-              />
+const Wrestlers = () => {
+  const router = useRouter();
+
+  // Redux
+  const dispatch = useDispatch();
+  const wrestlerState = useSelector((state: any) => state.wrestler);
+
+  const navigate = (wrestler: Wrestler) => {
+    dispatch(WrestlerActions.viewWrestler(wrestler));
+
+    const wrestlerName = wrestler.name.toLowerCase().replace(" ", "-");
+    router.push("wrestler/" + wrestlerName);
+  }
+
+  return (
+    <main className="flex flex-wrap content-center justify-center h-full sm:h-screen w-screen bg-zinc-50 sm:ml-20 sm:p-4">
+      {wrestlers.map((wrestler, idx) => {
+        return (
+          <div 
+            className={"wrestler " + wrestler.color} 
+            onClick={() => navigate(wrestler)}
+            key={idx}
+          >
+            <div className="wrestler-name-container">
+              <p className="wrestler-name">{wrestler.name}</p>
+              <p className="wrestler-nickname">{wrestler.nickname}</p>
             </div>
-          )
-        })}
-      </main>
-    </MainLayout>
+
+            <ExportedImage
+              className="wrestler-image"
+              src={wrestler.image}
+              alt={"An image of " + wrestler.name}
+              height={64}
+              width={64}
+            />
+          </div>
+        )
+      })}
+    </main>
   )
 }
 
